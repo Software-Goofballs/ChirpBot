a3bde4ffbfe7b812982d413ab5201b5646c0df56
TheLlamalegend <45502601+TheLlamalegend@users.noreply.github.com>
2022-01-27 12:38:34 -0600

    Update cheer.js (#4)

--- commands/cheer.js
+++ commands/cheer.js
@@ -1,18 +1,17 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
-const cheers = [
-	"yes yes yes yes yyyeeEEEESSSS!"
-];
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('cheer')
-		.setDescription('Cheers at people'),
-	async execute(interaction) {
-		let randoCheer;
-		randoCheer = cheers[Math.floor(Math.random() * cheers.length)];
-		randoCheer = randoCheer.replace(/USER/g, interaction.user.id);
-		
-		return interaction.reply(randoCheer);
+		.setDescription('Cheers at people')
+		.addStringOption(option =>
+				option
+					.setName('command')
+					.setRequired(true)
+					.addChoice('Yes', 'yes yes yes yes yyyeeEEEESSSS!')
+					.addChoice('Fuck', 'aww fuck aww fuck')
+				),
+	async execute(interaction) {		
+		return interaction.reply(interaction.options.getString('command'));
 	},
 };
